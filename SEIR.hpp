3ff1d034e14f3d1e38e51ef832025ee0b8b12643
Rao-mi <mariospaziante99@gmail.com>
2023-05-06 20:07:36 +0200

    Aggiornato SEIR.hpp

--- SEIR.hpp
+++ SEIR.hpp
@@ -1,1 +1,36 @@
-+
+#ifndef SEIR_HPP
+#define SEIR_HPP
+
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <stdexcept>
+
+
+struct SEIR {
+double S;
+double E;
+double I;
+double R;
+};
+
+
+class SEIR_model {
+ protected:
+    const float beta_;
+    const float gamma_;
+    const float mu_;
+    const float a_;
+    const unsigned int days_;
+    int N_;
+    std::vector<SEIR> history_;
+ public:
+    SEIR_model(SEIR SEIR_ini, const float beta, const float gamma, const float mu, const float a, const unsigned int days);
+    void verify();
+    void approx(int T);
+    void evolve();
+    void print();
+};
+
+
+#endif
