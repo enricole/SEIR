9de08a89498c17cc9b5109e0d3b368d69d68733c
Rao-mi <mariospaziante99@gmail.com>
2023-05-06 20:08:04 +0200

    Aggiunto SEIR_main.cpp

--- SEIR_main.cpp
+++ SEIR_main.cpp
@@ -1,1 +1,33 @@
+ #include "SEIR.hpp"
+using std::cout;
+using std::cin;
+using std::endl;
 
+ int main() {
+    double s,e,i,r;
+    double a,b,c,d;
+    int f;
+    cout<<"dimmi beta"<<endl;
+    cin>>a;
+    cout<<"dimmi gamma"<<endl;
+    cin>>b;
+    cout<<"dimmi mu"<<endl;
+    cin>>c;
+    cout<<"dimmi a"<<endl;
+    cin>>d;
+    cout<<"dimmi i giorni"<<endl;
+    cin>>f;
+    cout<<"dimmi i suscettibili"<<endl;
+    cin>>s;
+    cout<<"dimmi gli infetti"<<endl;
+    cin>>i;
+    cout<<"dimmi gli esposti"<<endl;
+    cin>>e;
+    cout<<"dimmi i curati"<<endl;
+    cin>>r;
+    SEIR seirprova{s, e, i, r};
+    SEIR_model modello1(seirprova, a, b, c, d, f);
+    modello1.approx(0);
+    modello1.print();
+    return 0;
+ }
