7eef0eebc07b16c56c9dcde658518114805c11d7
Enrico Lenzi <lenzi.enrico01@gmail.com>
2023-05-12 16:16:18 +0200

    Aggiunto operatore di uguaglianza

--- SEIR.hpp
+++ SEIR.hpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
-#include <fstream>   
+#include <fstream>
 
 // a struct definition in order to use more nimbly the parameters and confront them
 struct SEIR {
@@ -18,26 +18,28 @@ bool operator==(SEIR const& other) const noexcept {
 
 };
 
+// see file .cpp
 SEIR approx(SEIR non_approx);
 
 class SEIR_model {
  protected:
-    const float beta_; //transmission rate
-    const float gamma_; // healing and mortality rate caused by the illness
-    const float mu_; // natural mortality rate NOT caused by the illness
-    const float a_; // inverse of the incubation time of the illness
-    const int days_;
+    float beta_; //transmission rate
+    float gamma_; // healing and mortality rate caused by the illness
+    float mu_; // natural mortality rate NOT caused by the illness
+    float a_; // inverse of the incubation time of the illness
+    int days_;
     int N_; //the sum of SEIR parameters, that MUST stay constant
     std::vector<SEIR> history_; // a vector that recall the SEIR parameters for every day
  public:
  // function use is suggested by the name, can be deepened in the SEIR.cpp file
-    SEIR_model(SEIR SEIR_ini, const float beta, const float gamma, const float mu, const float a, const int days);
+    SEIR_model(SEIR SEIR_ini, float beta, float gamma, float mu, float a, int days);
     bool verify();
     void evolve();
     void evolve_runge_kutta();
     void print(int D);
     void print_out(std::string name);
-    SEIR daily_seir(int T);
+    SEIR daily_seir(int T); //used in the test file
+    SEIR_model& operator=(const SEIR_model& other); //ridefinition of = operator
 };
 
 
